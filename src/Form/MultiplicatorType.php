e226d2264945857953e67879ab8115c137d33fd7
gbrle <david.gabriele@outlook.fr>
2020-08-28 18:04:05 +0200

    change label at form romm & multiplicator

--- src/Form/MultiplicatorType.php
+++ src/Form/MultiplicatorType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Multiplicator;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,9 @@ class MultiplicatorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('value')
+            ->add('value', IntegerType::class, [
+                'label' => 'Valeur du multiplicateur'
+            ])
         ;
     }
 
--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Room;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,12 @@ class RoomType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('nameSpinAndGo')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la salle'
+            ])
+            ->add('nameSpinAndGo', TextType::class, [
+                'label' => 'Nom du spin and go'
+            ])
         ;
     }
 
