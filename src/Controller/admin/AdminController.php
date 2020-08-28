10e256f44ba5ceb21da2e1e399ddd4c19276212e
gbrle <david.gabriele@outlook.fr>
2020-08-28 03:21:46 +0200

    [FRONT] Add Pages And Links of Admin && Add Table for Users in User Page

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_home")
      */
     public function index()
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin/users", name="admin_users")
+     */
+    public function adminUsers()
+    {
+        return $this->render('admin/adminUsers.html.twig');
+    }
+
+    /**
+     * @Route("/admin/newsletter", name="admin_newsletter")
+     */
+    public function adminNewsLetter()
+    {
+        return $this->render('admin/adminNewsletter.html.twig');
+    }
+
+    /**
+     * @Route("/admin/addRoomIndex", name="admin_add_room_index")
+     */
+    public function adminAddRoom()
+    {
+        return $this->render('admin/adminAddRoomIndex.html.twig');
     }
 }
