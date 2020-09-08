af7d9a05d5e0619822f44324facab1ef179ca9c1
gbrle <david.gabriele@outlook.fr>
2020-09-08 12:24:38 +0200

    Sauv for swap PC

--- public/assets/user/js/addGame.js
+++ public/assets/user/js/addGame.js
@@ -1,6 +1,8 @@
 let blockAddGame = document.getElementById('blockAddGame');
 let blockAddGameRoom = document.getElementById('blockAddGameRoom');
 let blockRooms = document.getElementById('blockRooms');
+let blockAddBuyIn = document.getElementById('blockAddBuyIn');
+let blockBuyIn = document.getElementById('blockBuyIn');
 
 let dataAddGame = {}
 
@@ -23,13 +25,35 @@ function addRoom(id_room){
     dataAddGame.id_room = id_room;
 
     ajaxPost('/user/user_get_buy_in', id_room, function (response){
-        console.log((response))
-    })
-    setTimeout(function(){
-        blockAddGameRoom.style.display = 'none'
-        blockRooms.style.display = 'none'
 
-    }, 500);
+        Object.response(response).forEach(function (key) {
+          console.log(response[0][])
+        })
+
+
+        setTimeout(function(){
+            blockAddGameRoom.style.display = "none"
+            blockRooms.style.display = "none"
+
+            // I add all buy in in front (javascript only) I get the Json
+            let buyButtons = document.getElementById('buyButtons')
+
+
+            let button = document.createElement("button");
+            button.classList.add('paste')
+            button.innerText = "{{ room.id }}"
+
+            buyButtons.appendChild(button)
+
+            blockAddBuyIn.style.display = 'block'
+            blockBuyIn.style.display = 'block'
+
+
+
+        }, 500);
+
+
+    })
 
 
 }
