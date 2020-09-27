8d41c13ae540f4d4782abc7875fb8ca1c68308a4
gbrle <david.gabriele@outlook.fr>
2020-09-27 16:13:27 +0200

    Add Buy in moyen && Itm in controler && Front User page

--- src/Controller/user/AddGameController.php
+++ src/Controller/user/AddGameController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\user;
 
 use App\Entity\Game;
+use App\Entity\Room;
 use App\Repository\BuyInRepository;
 use App\Repository\GameRepository;
 use App\Repository\MultiplicatorRepository;
@@ -148,6 +149,62 @@ class AddGameController extends AbstractController
         ]);
 
 
+        // Tab empty for chart (for x Axis)
+        foreach ($games as $game){
+            array_push($nbreGameEmptyForChart, '');
+
+        }
+        $startGame = 0;
+        foreach ($games as $game){
+            array_push($nbreGame, $startGame + 1);
+            $startGame += 1;
+        }
+        $startBankroll = 0;
+        foreach ($games as $game){
+            array_push($bankroll, $startBankroll + ($game->getPrice() - $game->getBuyIn()));
+            $startBankroll = $startBankroll + ($game->getPrice() - $game->getBuyIn());
+        }
+
+        $brankroolFinale = 0;
+        $totalBuy = 0;
+        foreach ($games as $game){
+            $totalBuy = $totalBuy + $game->getBuyIn();
+            $brankroolFinale = $brankroolFinale + ($game->getPrice() - $game->getBuyIn());
+        }
+        $roi = ($brankroolFinale - $totalBuy) / $totalBuy;
+        $buyInMoyen = $totalBuy / count($nbreGame);
+
+        $itmCount = 0;
+        foreach ($games as $game){
+            if ($game->getPrice() > 0){
+                $itmCount = $itmCount + 1;
+            }
+        }
+        $itm = ($itmCount * 100) / count($nbreGame);
+
+        if ($request->isXMLHttpRequest()) {
+            return new JsonResponse(([json_encode($nbreGame), json_encode($bankroll), round($roi, 1), json_encode($nbreGameEmptyForChart), round($buyInMoyen, 1), round($itm, 1)]));
+        }
+
+
+        return new Response('Error 400', 400);
+    }
+
+    /**
+     * @Route("/user/user_get_games_by_room_for_chart/{id}", name="user_get_games_by_room_for_chart")
+     */
+    public function get_games_by_room_for_chart(Room $room, Request $request, GameRepository $gameRepository)
+    {
+        $nbreGame = [];
+        $bankroll = [];
+        $nbreGameEmptyForChart = [];
+
+        $games = $gameRepository->findBy([
+            'user' => $this->getUser(),
+            'room' => $room->getName()
+        ]);
+
+
         // Tab empty for chart
         foreach ($games as $game){
             array_push($nbreGameEmptyForChart, '');
@@ -171,9 +228,18 @@ class AddGameController extends AbstractController
             $brankroolFinale = $brankroolFinale + ($game->getPrice() - $game->getBuyIn());
         }
         $roi = ($brankroolFinale - $totalBuy) / $totalBuy;
+        $buyInMoyen = $totalBuy / count($nbreGame);
+
+        $itmCount = 0;
+        foreach ($games as $game){
+            if ($game->getPrice() > 0){
+                $itmCount = $itmCount + 1;
+            }
+        }
+        $itm = ($itmCount * 100) / count($nbreGame);
 
         if ($request->isXMLHttpRequest()) {
-            return new JsonResponse(([json_encode($nbreGame), json_encode($bankroll), round($roi, 1), json_encode($nbreGameEmptyForChart)]));
+            return new JsonResponse(([json_encode($nbreGame), json_encode($bankroll), round($roi, 1), json_encode($nbreGameEmptyForChart), round($buyInMoyen), round($itm)]));
         }
 
 
--- src/Controller/user/UserController.php
+++ src/Controller/user/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\user;
 
+use App\Entity\Room;
 use App\Repository\GameRepository;
 use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +23,24 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'rooms' => $rooms,
             'games' => $games,
+        ]);
+    }
+
+    /**
+     * @Route("/user/stats-by-room/{id}", name="user_stats_by_room")
+     */
+    public function statsByRoom(Room $room, RoomRepository $roomRepository, GameRepository $gameRepository)
+    {
+        $rooms = $roomRepository->findAll();
+        $games = $gameRepository->findBy([
+            'user' => $this->getUser(),
+            'room' => $room->getName()
+        ]);
+
+        return $this->render('user/statsByRoom.html.twig', [
+            'rooms' => $rooms,
+            'games' => $games,
+            'room' => $room
         ]);
     }
 }
